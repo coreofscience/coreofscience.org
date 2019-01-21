6c258f4f3b03d0bc546ee0ea4650907566cea0ea
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2019-01-21 00:27:48 -0500

    Take care of the actual favico

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
