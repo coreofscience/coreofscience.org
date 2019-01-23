48df8f650809e5dcc0963d3b15a7ee366c314e4f
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2019-01-22 19:44:33 -0500

    Set the site under construction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from "react";
+import "./index.css";
 
-export default () => <div>Hello world!</div>
+const UnderConstruction = () => (
+  <div className="UnderConstruction">
+    <img
+      className="UnderConstruction-logo"
+      src="/images/logo.png"
+      alt="Core of science"
+    />
+    <h1>Core of Science</h1>
+    <p>Under construction.</p>
+  </div>
+);
+
+export default UnderConstruction;
