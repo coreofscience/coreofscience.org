c425e271b3294a54f65bded7ba9f036b8ae6bb54
Oscar Arbeláez <odarbelaeze@gmail.com>
2019-06-27 18:28:28 -0500

    Connect the email form

--- src/components/email.js
+++ src/components/email.js
@@ -1,27 +1,38 @@
 import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
+import crypto from "crypto";
+
 import FirebaseContext from "../context/firebase";
 
 const EmailForm = ({ className }) => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState();
   const firebase = useContext(FirebaseContext);
+  const hash = crypto
+    .createHash("md5")
+    .update(email)
+    .digest("hex")
+    .slice(0, 20);
   const storeEmail = () => {
     if (!firebase.ready) return;
-    firebase.app
-      .firestore()
-      .collection("emails")
-      .add({ email, timestamp: new Date() })
-      .then(() => setEmail(""));
+    firebase.firestore
+      .doc(`emails/${hash}`)
+      .set({ email, timestamp: new Date() })
+      .then(() => setEmail(""))
+      .catch(() => setError("Invalid email"));
   };
   return (
     <div className={className}>
       <div className="control control-expanded">
         <input
-          className="input"
+          className={`input ${error ? "error" : ""}`}
           type="email"
           name="email"
           value={email}
-          onChange={e => setEmail(e.target.value)}
+          onChange={e => {
+            setEmail(e.target.value);
+            setError();
+          }}
           placeholder="example@email.com"
         />
       </div>
