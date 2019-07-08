f810f09e9efd7d42ec0276a949cb73af3d08efb3
jdalzatec <jdalzatec@unal.edu.co>
2019-07-08 15:00:13 -0500

    Add message when an user registers his/her email

--- src/components/email.js
+++ src/components/email.js
@@ -7,6 +7,7 @@ import FirebaseContext from "../context/firebase";
 const EmailForm = ({ className }) => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState();
+  const [message, setMessage] = useState("");
   const firebase = useContext(FirebaseContext);
   const hash = crypto
     .createHash("md5")
@@ -18,32 +19,59 @@ const EmailForm = ({ className }) => {
     firebase.firestore
       .doc(`emails/${hash}`)
       .set({ email, timestamp: new Date() })
-      .then(() => setEmail(""))
+      .then(() => {
+        setEmail("");
+        setMessage("Thanks for registering");
+      })
       .catch(() => setError("Invalid email"));
   };
+  const resetMessage = () => {
+    setMessage("");
+  };
+
   return (
-    <div className={className}>
-      <div className="control control-expanded">
-        <input
-          className={`input ${error ? "error" : ""}`}
-          type="email"
-          name="email"
-          value={email}
-          onChange={e => {
-            setEmail(e.target.value);
-            setError();
-          }}
-          placeholder="example@email.com"
-        />
-      </div>
-      <div className="control">
-        <button
-          onClick={storeEmail}
-          className="button button-primary button-block button-shadow"
-        >
-          Keep in touch
-        </button>
-      </div>
+    <div>
+      {message ? (
+        <div>
+          <h5
+            style={{
+              color: "rgb(255, 55, 94)"
+            }}
+          >
+            {message}
+          </h5>
+          <button
+            onClick={resetMessage}
+            className="button button-primary button-block button-shadow"
+          >
+            Register other email
+          </button>
+        </div>
+      ) : (
+        <div className={className}>
+          <div className="control control-expanded">
+            <input
+              className={`input ${error ? "error" : ""}`}
+              type="email"
+              name="email"
+              value={email}
+              onChange={e => {
+                setEmail(e.target.value);
+                setError();
+              }}
+              placeholder="example@email.com"
+            />
+          </div>
+          <div className="control">
+            <button
+              onClick={storeEmail}
+              className="button button-primary button-block button-shadow"
+            >
+              Keep in touch
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
