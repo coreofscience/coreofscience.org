71e1ea0a0e34de4ff24ae0ea9b90f1f8b5575520
Oscar Arbelaez <oscar@arbelaez.dev>
2021-08-25 10:59:52 -0500

    Try to enable analytics via firebase

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -5,6 +5,7 @@
 import React, { useEffect, useState, createContext } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/analytics";
 
 const FirebaseContext = createContext(null);
 
@@ -12,8 +13,9 @@ const FirebaseProvider = ({ children }) => {
   const [state, setState] = useState({
     app: null,
     firestore: null,
-    ready: false
+    ready: false,
   });
+
   useEffect(() => {
     if (typeof window === "undefined") return;
     if (process.env.NODE_ENV === "development") {
@@ -24,28 +26,31 @@ const FirebaseProvider = ({ children }) => {
         projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
         storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.GATSBY_FIREBASE_APP_ID
+        appId: process.env.GATSBY_FIREBASE_APP_ID,
       };
       firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
       setState({
         app: firebase,
         firestore: firebase.firestore(),
-        ready: true
+        ready: true,
       });
     } else {
       fetch("/__/firebase/init.json")
-        .then(async response => {
+        .then(async (response) => {
           firebase.initializeApp(await response.json());
+          firebase.analytics();
         })
         .then(() =>
           setState({
             app: firebase,
             firestore: firebase.firestore(),
-            ready: true
+            ready: true,
           })
         );
     }
   }, []);
+
   return (
     <FirebaseContext.Provider value={state}>
       {children}
