69034fe57cda39fe29838509ef97e3ef8d4677e1
Oscar Arbelaez <oscar@arbelaez.dev>
2023-01-02 15:11:48 -0500

    Get our header going on

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"brand-green": "rgb(var(--color-brand-green) / <alpha-value>)",
+				"brand-yellow": "rgb(var(--color-brand-yellow) / <alpha-value>)",
+				"brand-blue": "rgb(var(--color-brand-blue) / <alpha-value>)",
+			}
+		},
 	},
 	plugins: [],
 }
